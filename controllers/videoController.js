c410b5411667dae45d3795d36b5bf35e3cbb7433
KiJun Park <ekdlwns3685@naver.com>
2019-07-24 18:23:37 +0900

    added video upload

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,8 +1,14 @@
-import { videos } from "../db";
+import Video from "../models/Video";
 import routes from "../routes";
 
-export const home = (req, res) => {
-  res.render("home", { pageTitle: "Home", videos });
+export const home = async (req, res) => {
+  try {
+    const videos = await Video.find({});
+    res.render("home", { pageTitle: "Home", videos });
+  } catch (error) {
+    console.log(error);
+    res.render("home", { pageTitle: "Home", videos: [] });
+  }
 };
 
 export const search = (req, res) => {
@@ -12,14 +18,23 @@ export const search = (req, res) => {
   res.render("search", { pageTitle: "Search", searchingBy, videos });
 };
 export const getUpload = (req, res) =>
-  res.render("upload", { pageTitle: "Uploads" });
+  res.render("upload", { pageTitle: "Upload" });
 
-export const postUpload = (req, res) => {
+export const postUpload = async (req, res) => {
   const {
-    body: { file, title, description }
+    body: { title, description },
+    file: { path }
   } = req;
+
+  const newVideo = await Video.create({
+    fileUrl: path,
+    title,
+    description
+  });
+  console.log(newVideo);
   // TO Do: Upload and save video
-  res.redirect(routes.videoDetail(234243));
+  //res.render("upload", { pageTitle: "Upload" });
+  res.redirect(routes.videoDetail(newVideo.id));
 };
 
 export const videoDetail = (req, res) =>
